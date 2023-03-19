9e6050d01d8a62ddf9488291d3d70d6c784c3c54
Kavata-D <kavatakelly@gmail.com>
2023-03-19 12:24:30 -0700

    added exercise scripts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* This is the main entry point */
+	char string[300] = "\"Programming is like building a multilingual puzzle";
+
+	puts(string);
+	return (0);
+}
+
+
